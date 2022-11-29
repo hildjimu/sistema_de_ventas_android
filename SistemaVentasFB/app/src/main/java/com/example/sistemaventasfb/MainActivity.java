c9928432e1e6793372561216fe134045c3482a24
505 <505@academicos.cesde.edu.co>
2022-11-29 18:01:42 -0500

    guardar y modificar y buscar

--- SistemaVentasFB/app/src/main/java/com/example/sistemaventasfb/MainActivity.java
+++ SistemaVentasFB/app/src/main/java/com/example/sistemaventasfb/MainActivity.java
@@ -10,10 +10,14 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,6 +25,7 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
     //se genera un objeto para conectarse a la base de datos de firebase - firebase
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String idAutomatic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,43 +44,132 @@ public class MainActivity extends AppCompatActivity {
         ImageButton btnsales = findViewById(R.id.btnsales);
         ImageButton btnlist = findViewById(R.id.btnlist );
         //Eventos
-        btnsave.setOnClickListener(new View.OnClickListener() {
+        btnedit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                //Validación de que los datos esten bien diligenciados
-                String midseller = idseller.getText().toString();
-                String mfullname = fullname.getText().toString();
-                String memail = email.getText().toString();
-                String mpassword = password.getText().toString();
+            public void onClick(View v) {
+                Map<String, Object> mSeller = new HashMap<>();
+                mSeller.put("idseller", idseller.getText().toString());
+                mSeller.put("fullname", fullname.getText().toString());
+                mSeller.put("email", email.getText().toString());
+                mSeller.put("password", password.getText().toString());
+                mSeller.put("totalcomision", 0);
+                //Actualizar el documento a la seleccion seller a traves de la tabla temporal mseller
+                db.collection("seller").document(idAutomatic)
+                                                .set(mSeller)
+                                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                    @Override
+                                                    public void onSuccess(Void unused) {
+                                                        Toast.makeText(getApplicationContext(), "Vendedor actulizado correctamente", Toast.LENGTH_SHORT).show();
+                                                        idseller.setText("");
+                                                        fullname.setText("");
+                                                        email.setText("");
+                                                        password.setText("");
+                                                    }
+                                                })
+                                                .addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        Toast.makeText(getApplicationContext(), "Error al guardar los datos del vendedor...", Toast.LENGTH_SHORT).show();
+                                                    }
+                                                });
+                                    }
+                                });
 
-                if(!midseller.isEmpty() && !mfullname.isEmpty() && !memail.isEmpty() && !mpassword.isEmpty()) {
-                    //Crear una tabla temporal con los mismos campos de la coleccion seller
-                    Map<String, Object> mSeller = new HashMap<>();
-                    mSeller.put("idseller", midseller);
-                    mSeller.put("fullname", mfullname);
-                    mSeller.put("email", memail);
-                    mSeller.put("password", mpassword);
-                    mSeller.put("totalcomision", 0);
-                    //Agregar el documento a la coleccion seller a través de la tabla temporal seller
-                    db.collection("seller")
-                            .add(mSeller)
-                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                                @Override
-                                public void onSuccess(DocumentReference documentReference) {
-                                    Toast.makeText(getApplicationContext(), "Datos ingresados correctamente", Toast.LENGTH_SHORT).show();
-                                }
-                            })
-                            .addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Toast.makeText(getApplicationContext(), "Error al guardar los datos del vendedor...", Toast.LENGTH_SHORT).show();
-                                }
-                            });
-                }
-                else{
-                    Toast.makeText(getApplicationContext(), "Por favor diligenciar todos campos...",Toast.LENGTH_SHORT).show();
-                }
+                btnsearch.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //Buscar por idseller y recuperar todos los datos
+                        db.collection("seller")
+                                .whereEqualTo("idseller", idseller.getText().toString())
+                                .get()
+                                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                        if (task.isSuccessful()) {
+                                            if (!task.getResult().isEmpty()) {
+                                                //la instancia tiene información del documento
+                                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                                    idAutomatic = document.getId();
+                                                    //mostrar la informacion en cada uno de los objetos referenciados
+                                                    fullname.setText(document.getString("fullname"));
+                                                    email.setText(document.getString("email"));
+                                                    totalcomision.setText(String.valueOf(document.getDouble("totalcomision")));
+                                                }
+                                            } else {
+                                                //si no encuentra el idseller del vendedor
+                                                Toast.makeText(getApplicationContext(), "La identificacion ingresada no existe...", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    }
+                                });
+                    }
+                });
+                btnedit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getApplicationContext(),"Entra por aqui...",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                btnsave.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //Validación de que los datos esten bien diligenciados
+                        String midseller = idseller.getText().toString();
+                        String mfullname = fullname.getText().toString();
+                        String memail = email.getText().toString();
+                        String mpassword = password.getText().toString();
+
+                        if (!midseller.isEmpty() && !mfullname.isEmpty() && !memail.isEmpty() && !mpassword.isEmpty()) {
+                            //Crear una tabla temporal con los mismos campos de la coleccion seller
+                            Map<String, Object> mSeller = new HashMap<>();
+                            mSeller.put("idseller", midseller);
+                            mSeller.put("fullname", mfullname);
+                            mSeller.put("email", memail);
+                            mSeller.put("password", mpassword);
+                            mSeller.put("totalcomision", 0);
+
+                            db.collection("seller")
+                                    .whereEqualTo("idseller", idseller.getText().toString())
+                                    .get()
+                                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                            if (task.isSuccessful()) {
+                                                if (!task.getResult().isEmpty()) {
+                                                    //la instancia tiene información del documento
+                                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                                        Toast.makeText(getApplicationContext(), "Usuario Existente", Toast.LENGTH_SHORT).show();
+                                                    }
+                                                } else {
+                                                    //Agregar el documento a la coleccion seller a través de la tabla temporal seller
+                                                    db.collection("seller")
+                                                            .add(mSeller)
+                                                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                                                @Override
+                                                                public void onSuccess(DocumentReference documentReference) {
+                                                                    Toast.makeText(getApplicationContext(), "Datos ingresados correctamente", Toast.LENGTH_SHORT).show();
+                                                                    idseller.setText("");
+                                                                    fullname.setText("");
+                                                                    email.setText("");
+                                                                    password.setText("");
+                                                                }
+                                                            })
+                                                            .addOnFailureListener(new OnFailureListener() {
+                                                                @Override
+                                                                public void onFailure(@NonNull Exception e) {
+                                                                    Toast.makeText(getApplicationContext(), "Error al guardar los datos del vendedor...", Toast.LENGTH_SHORT).show();
+                                                                }
+                                                            });
+                                                }
+                                            }
+                                        }
+                                    });
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Por favor diligenciar todos campos...", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
-        });
-    }
-}
+        }
+
+
